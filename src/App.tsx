aa2e80f727f044000791643b930e58cd0c99b025
FayizIT <fayizcj94@gmail.com>
2024-09-22 20:23:51 +0530

    app setup

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+// import About from "./Components/About";
+// import Home from "./Components/Home";
+// import Navbar from "./Components/Navbar";
+// import SocialLink from "./Components/SocialLink";
+// import Skills from "./Components/Skills";
+// import Contact from "./Components/Contact";
+// import Works from "./Components/Works";
+
+const App: React.FC = () => {
+  return (
+    <div>
+      {/* <Navbar />
+      <Home />
+      <About />
+      <Skills />
+      <Works />
+      <Contact />
+      <SocialLink /> */}
+      <h1>Hello World</h1>
+    </div>
+  );
+};
+
+export default App;
